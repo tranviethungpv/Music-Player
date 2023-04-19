85f192fef2cda3c93f5df3cd053d3f9e9f814cdb
truongqh <truongnqt02@gmail.com>
2023-04-19 23:17:35 +0700

    niệm lần1

--- app/src/main/java/com/example/musicplayer/views/Home.kt
+++ app/src/main/java/com/example/musicplayer/views/Home.kt
@@ -1,18 +1,53 @@
 package com.example.musicplayer.views
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.musicplayer.R
+import com.example.musicplayer.activity.Songadapter
+
+import com.example.musicplayer.model.Song
+import com.example.musicplayer.viewmodels.SongViewModel
 
 
 class Home : Fragment() {
+    private lateinit var songAdapter:Songadapter
+    private lateinit var recyclerViewListSong: RecyclerView
+    private lateinit var songViewModel: SongViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        recyclerViewListSong = view.findViewById(R.id.recyclerListSongs)
+        recyclerViewListSong.layoutManager = GridLayoutManager(activity, 2)
+        val dividerDecoration = DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL)
+        recyclerViewListSong.addItemDecoration(dividerDecoration)
+
+        songViewModel = ViewModelProvider(this)[SongViewModel::class.java]
+        songViewModel.allSongs.observe(viewLifecycleOwner, Observer {
+            if (it.isNotEmpty()) {
+                val songList = it
+                songAdapter = Songadapter(songList)
+                recyclerViewListSong.adapter = songAdapter
+            }
+        })
+        recyclerViewListSong.setOnClickListener {
+
+        }
+        return view
     }
 }
