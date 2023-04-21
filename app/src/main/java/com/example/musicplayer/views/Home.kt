67f306e24450ccf4577e755fe3a1abf47951a983
truongqh <truongnqt02@gmail.com>
2023-04-22 00:54:13 +0700

    view

--- app/src/main/java/com/example/musicplayer/views/Home.kt
+++ app/src/main/java/com/example/musicplayer/views/Home.kt
@@ -4,19 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.ViewFlipper
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.viewbinding.ViewBindings
 import com.example.musicplayer.R
 import com.example.musicplayer.activity.adapter.SongSearchadapter
 import com.example.musicplayer.activity.adapter.Songadapter
 import com.example.musicplayer.viewmodels.SongViewModel
+import com.squareup.picasso.Picasso
 
 
 class Home : Fragment() {
@@ -27,14 +30,14 @@ class Home : Fragment() {
     private lateinit var imgSearch:ImageView
     private lateinit var hint:TextView
     private lateinit var layoutSearch:RecyclerView
+    //var  viewFlipper = findViewById<ViewFlipper>(R.id.viewFlipper)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        val view = inflater.inflate(R.layout.fragment_home, container, false)
-
-        recyclerViewListSong = view.findViewById(R.id.recyclerListSongs)
+        val view = inflater.inflate(com.example.musicplayer.R.layout.fragment_home, container, false)
+        recyclerViewListSong = view.findViewById(com.example.musicplayer.R.id.recyclerListSongs)
         recyclerViewListSong.layoutManager = GridLayoutManager(activity, 2)
         val dividerDecoration = DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL)
         recyclerViewListSong.addItemDecoration(dividerDecoration)
@@ -50,31 +53,31 @@ class Home : Fragment() {
         recyclerViewListSong.setOnClickListener {
 
         }
+        ActionViewFlipper()
        // SearchByHint()
         return view
     }
 
-//    private fun SearchByHint() {
-//        imgSearch.setOnClickListener(View.OnClickListener() {
-//            @Override
-//             fun onClick(v:View){
-//                recyclerViewListSong = v.findViewById(R.id.layout_search)
-//                recyclerViewListSong.layoutManager = GridLayoutManager(activity, 1)
-//                val dividerDecoration = DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL)
-//                recyclerViewListSong.addItemDecoration(dividerDecoration)
-//
-//                songViewModel = ViewModelProvider(this)[SongViewModel::class.java]
-//                songViewModel.songSearch.observe(viewLifecycleOwner, Observer {
-//                    if (it.isNotEmpty()) {
-//                        val songList = it
-//                        songListSeach = SongSearchadapter(songList)
-//                        recyclerViewListSong.adapter = songListSeach
-//                    }
-//                })
-//
-//            }
-//        });
-//
-//
-//    }
+    private fun ActionViewFlipper() {
+        val viewFlipper = getView()?.findViewById<ViewFlipper>(R.id.viewflipper)
+        val imageList = mutableListOf(
+            R.drawable.ic_skype,
+            R.drawable.ic_facebook,
+            R.drawable.ic_gmail
+        )
+
+        // Thêm các ảnh vào ViewFlipper
+        imageList.forEach { image ->
+            val imageView = ImageView(requireContext())
+            imageView.setImageResource(image)
+            imageView.scaleType = ImageView.ScaleType.FIT_XY
+            viewFlipper?.addView(imageView)
+        }
+        viewFlipper?.setFlipInterval(5000)
+        viewFlipper?.setAutoStart(true)
+        viewFlipper?.setInAnimation(requireContext(), android.R.anim.fade_in)
+        viewFlipper?.setOutAnimation(requireContext(), android.R.anim.fade_out)
+    }
+
+
 }
--- app/src/main/java/com/example/musicplayer/views/NoticeMusic.kt
+++ app/src/main/java/com/example/musicplayer/views/NoticeMusic.kt
@@ -5,14 +5,42 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.musicplayer.R
+import com.example.musicplayer.activity.adapter.SongSearchadapter
+import com.example.musicplayer.activity.adapter.Songadapter
+import com.example.musicplayer.viewmodels.SongViewModel
 
 class NoticeMusic : Fragment() {
+    private lateinit var recyclerViewListSong: RecyclerView
+    private lateinit var songViewModel: SongViewModel
+    private lateinit var songListSeach: SongSearchadapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new, container, false)
+        val view = inflater.inflate(R.layout.fragment_new, container, false)
+        recyclerViewListSong = view.findViewById(R.id.recyclerListSongs)
+        recyclerViewListSong.layoutManager = GridLayoutManager(activity, 1)
+        val dividerDecoration = DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL)
+        recyclerViewListSong.addItemDecoration(dividerDecoration)
+        songViewModel = ViewModelProvider(this)[SongViewModel::class.java]
+        songViewModel.allSongs.observe(viewLifecycleOwner, Observer {
+            if (it.isNotEmpty()) {
+                val songList = it
+                songListSeach = SongSearchadapter(songList)
+                recyclerViewListSong.adapter = songListSeach
+            }
+        })
+
+        recyclerViewListSong.setOnClickListener {
+
+        }
+        // SearchByHint()
+        return view
     }
 }
